45d6bcdb65c841293c9e44e5da5ac7a175b0e760
Tony <tonywzhang@gmail.com>
2018-09-10 14:41:46 -0700

    updated w/ solution

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,10 +1,7 @@
-# There are many ways to implement these methods, feel free to add arguments
-# to methods as you see fit, or to create helper methods.
-
-require_relative 'bst_node'
+require 'bst_node'
+require 'byebug'
 
 class BinarySearchTree
-
   attr_reader :root
 
   def initialize
@@ -12,75 +9,168 @@ class BinarySearchTree
   end
 
   def insert(value)
-    @root = recurse_insert(@root, value)
+    # setting the root here in case it is still nil
+    @root = insert_into_tree(@root, value)
   end
 
   def find(value, tree_node = @root)
     return nil if tree_node.nil?
     return tree_node if tree_node.value == value
-    if tree_node.value > value
+
+    if value < tree_node.value
       find(value, tree_node.left)
-    else
+    elsif value > tree_node.value
       find(value, tree_node.right)
     end
   end
 
-  #Step 1. Perform find(val).
-  #Step 2. If val has no children, "erase" val.
-  #Step 3. If val has one child, promote this child to take val's place, keeping everything else the same.
-  #Step 4a. If val has two children, find the maximum element in val's left subtree. Call this node r.
-  #Step 4b. Replace val with r.
-  #Step 4c. If r had a left child, promote this child to take r's place. Keep everything else the same.
-
   def delete(value)
-    node = find(value)
-    if node.right.nil? && node.left.nil?
-      node.parent.left = nil
-      node.parent.right = nil
-    elsif node.left.nil? && node.right
-      node = node.right
-    elsif node.right.nil? && node.left
-      node = node.left
-    else
-
-    end
-
-    node
+    @root = delete_from_tree(@root, value)
   end
 
-  # helper method for #delete:
   def maximum(tree_node = @root)
-    return tree_node if tree_node.right == nil
-    maximum(tree_node.right)
+    if tree_node.right
+      maximum_node = maximum(tree_node.right)
+    else
+      maximum_node = tree_node
+    end
+    maximum_node
   end
 
   def depth(tree_node = @root)
+    if tree_node.nil?
+      return -1;
+    else
+      left_depth = depth(tree_node.left)
+      right_depth = depth(tree_node.right)
 
+      if left_depth > right_depth
+        return left_depth + 1
+      else
+        return right_depth + 1
+      end
+    end
   end
 
   def is_balanced?(tree_node = @root)
+    return true if tree_node.nil?
 
+    balanced = true
+    left_depth = depth(tree_node.left)
+    right_depth = depth(tree_node.right)
+    balanced = false if (left_depth - right_depth).abs > 1
+
+    if balanced && is_balanced?(tree_node.left) && is_balanced?(tree_node.right)
+      return true
+    end
+
+    false
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    # left children, itself, right children
+    if tree_node.left
+      in_order_traversal(tree_node.left, arr)
+    end
+
+    arr.push(tree_node.value)
+
+    if tree_node.right
+      in_order_traversal(tree_node.right, arr)
+    end
+
+    arr
   end
+
 
 
   private
-  # optional helper methods go here:
 
-  def recurse_insert(node, val)
-    if node == nil
-      return BSTNode.new(val)
+  def insert_into_tree(tree_node, value)
+    # return the tree with the node inserted
+
+    return BSTNode.new(value) if tree_node.nil?
+
+    if value <= tree_node.value
+      tree_node.left = insert_into_tree(tree_node.left, value)
+    elsif value > tree_node.value
+      tree_node.right = insert_into_tree(tree_node.right, value)
     end
 
-    if val < node.value
-      node.left = recurse_insert(node.left, val)
-    else
-      node.right = recurse_insert(node.right, val)
-    end
-
-    node
+    tree_node
   end
 
+  def delete_from_tree(tree_node, value)
+    # return subtree with element removed
+
+    # if correct node is found, then call remove on it.
+    # otherwise, recurse until you find it
+
+    if value < tree_node.value
+      tree_node.left = delete_from_tree(tree_node.left, value)
+    elsif value > tree_node.value
+      tree_node.right = delete_from_tree(tree_node.right, value)
+    elsif value == tree_node.value
+      tree_node = remove(tree_node)
+    end
+
+    tree_node
+  end
+
+  def remove(node)
+    # return subtree with element removed
+
+    if node.left.nil? && node.right.nil?
+      # when node doesn't have children,
+      # simply remove it.
+      return nil
+    elsif node.left && node.right.nil?
+      # when node only has one child,
+      # delete it and promote its child to take its place.
+      return node.left
+    elsif node.left.nil? && node.right
+      return node.right
+    else
+      # if node has two children,
+      # promote the maximum node in its left subtree to replace itself
+      # if that specific node that was promoted has children,
+      # then promote
+      return replace_parent(node)
+    end
+  end
+
+  def replace_parent(parent)
+    # parent has two children
+    # 1 needs to be promoted
+
+    replacement_node = maximum(parent.left)
+    if replacement_node.left
+      promote_child(parent.left)
+    end
+
+    # since the replacement_node is still pointing to its old children,
+    # we have to update its children pointers
+    replacement_node.left = parent.left
+    replacement_node.right = parent.right
+
+    replacement_node
+  end
+
+  def promote_child(tree_node)
+    # find largest and take it out
+    # largest is guaranteed to have at most 1 child
+    # so promotion is simple
+
+    if tree_node.right
+      parent = tree_node
+      child = tree_node.right
+
+      while child.right
+        parent = parent.right
+        child = child.right
+      end
+
+      parent.right = child.left
+    end
+  end
 end
